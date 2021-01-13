8b49af5b3dfbb6bcf808120da7bebaa24d7f3737
Dishank Poddar <dishankpodar@gmail.com>
2021-01-13 17:03:20 +0530

    static files issue fixed

--- Quizapp/settings.py
+++ Quizapp/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_files")
+]
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'question-page'
